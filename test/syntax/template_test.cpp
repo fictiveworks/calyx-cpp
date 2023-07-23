786ddee43a11471cd35c5285e10e9796d65d06e4
TheDeathlyCow <thedeathlycow@gmail.com>
2023-07-23 15:28:07 +1200

    template node fragment tests

--- test/syntax/template_test.cpp
+++ test/syntax/template_test.cpp
@@ -7,11 +7,58 @@
 
 using namespace calyx;
 
-TEST_CASE("Fragmenting tokens includes delimiters") 
+/**
+ * @brief Run a fragmentation test. Fragments the {@code raw} input into a list of tokens and asserts that it is equal to
+ * the {@code expected} token list
+ *
+ * @param raw The raw input to fragmentize
+ * @param expected The expected fragmentation output
+ */
+void testFragmentation(const std::string&& raw, const std::vector<std::string> expected)
 {
-    std::string raw = "{token} words {token}";
     std::vector<std::string> fragments = TemplateNode::fragmentString(raw);
-
-    std::vector<std::string> expected = { "{token}", " words ", "{token}" };
     REQUIRE(expected == fragments);
 }
+
+TEST_CASE("Fragmenting tokens")
+{
+    SECTION("Fragmenting tokens includes expressions and atoms")
+    {
+        testFragmentation("{expression} atom {expression}", { "{expression}", " atom ", "{expression}" });
+    }
+
+    SECTION("Fragmenting tokens starting with atoms")
+    {
+        testFragmentation("start atom {expression} atom {expression}", { "start atom ", "{expression}", " atom ", "{expression}" });
+    }
+
+    SECTION("Fragmenting tokens two space-separated expressions")
+    {
+        testFragmentation("{expression} {expression}", { "{expression}", " ", "{expression}" });
+    }
+
+    SECTION("Fragmenting tokens two space-separated expressions")
+    {
+        testFragmentation("{expression} {expression}", { "{expression}", " ", "{expression}" });
+    }
+
+    SECTION("Fragmenting tokens just expressions")
+    {
+        testFragmentation("{expression1}{expression2}", { "{expression1}", "{expression2}" });
+    }
+
+    SECTION("Fragmenting tokens just atoms")
+    {
+        testFragmentation("this is just a single atom", { "this is just a single atom" });
+    }
+
+    SECTION("Fragmenting tokens empty input")
+    {
+        testFragmentation("", { });
+    }
+}
+
+
+
+
+
